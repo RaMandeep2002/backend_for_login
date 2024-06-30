571d57b81acd1659f95ff2a9616c8d495d51a63e
Ramandeep singh <ramandeepsingh@192.168.1.5>
2024-06-30 11:26:04 +0530

    add a hash password

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,7 +1,7 @@
 const userModel = require('../models/users');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const SECRET_KEY = 'Raman';
+const SECRET_KEY = 'deep';
 const signup = async (req, resp) => {
   const { username, email, password } = req.body;
   try {
@@ -13,18 +13,18 @@ const signup = async (req, resp) => {
       return resp.status(400).json({ message: 'user already exist' });
     }
 
-    // const hashpass = await bcrypt.hash(password, 10);
+    const hashpass = await bcrypt.hash(password, 10);
 
-    // const result = await userModel.create({
-    //   email: email,
-    //   password: hashpass,
-    //   username: username,
-    // });
     const result = await userModel.create({
       email: email,
-      password: password,
+      password: hashpass,
       username: username,
     });
+    // const result = await userModel.create({
+    //   email: email,
+    //   password: password,
+    //   username: username,
+    // });
 
     const token = await jwt.sign(
       { email: result.email, id: result._id },
@@ -48,19 +48,22 @@ const signin = async (req, resp) => {
       return resp.status(404).json({ message: 'user not found' });
     }
 
-    // const matchpassword = await bcrypt.compare(password, exstinguser.password);
-    // if (!matchpassword) {
-    //   return resp.status(400).json({ message: 'Invaild credentail' });
-    // }
-    if (!password) {
+    const matchpassword = await bcrypt.compare(password, exstinguser.password);
+    if (!matchpassword) {
       return resp.status(400).json({ message: 'Invaild credentail' });
     }
+    // if (!password) {
+    //   return resp.status(400).json({ message: 'Invaild credentail' });
+    // }
 
     const token = await jwt.sign(
       { email: exstinguser.email, id: exstinguser._id },
       SECRET_KEY
     );
-    resp.status(201).json({ exstinguser: exstinguser, token: token });
+    resp.status(201).json({
+      exstinguser: exstinguser,
+      token: token,
+    });
   } catch (error) {
     console.log(error);
     resp.status(500).json({ message: 'Something happen on Server' });
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,6 +5,6 @@ const auth = require('../middlewares/auth');
 const userRouter = express.Router();
 
 userRouter.post('/signup', signup);
-userRouter.post('/signin', signin);
+userRouter.post('/signin',  signin);
 
 module.exports = userRouter;
